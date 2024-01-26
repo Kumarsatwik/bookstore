dcd9c2a3c65585018af25ea7849aab22f5b37110
kumarsatwik <38569860+Kumarsatwik@users.noreply.github.com>
2024-01-26 19:44:42 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,11 +7,9 @@ const Footer = () => {
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <Link className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <span className="ml-3 text-xl">
-              <img
-                src="https://www.figma.com/file/Cs9CkR0rPFVSmUpVZFTZXo/image/45895a848aabf1bdde6f35265ff40fb98689a2d3"
-                alt=""
-                className=" object-cover h-[100px] w-[100px]"
-              />
+              <Link to="/>
+                <p className="text-gray-500 text-xl font-bold">Book</p>
+                </Link>
             </span>
           </Link>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
