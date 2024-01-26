4afb184d2b3480978acb1926ae071e08abb45357
kumarsatwik <38569860+Kumarsatwik@users.noreply.github.com>
2024-01-26 18:16:42 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/bookapi")
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("MongoDB connected");
   })
